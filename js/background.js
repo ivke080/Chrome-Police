8a6ab437916de3403038a37c156dccfe78dd60bc
ivke080 <ivke11080@gmail.com>
2016-08-12 17:20:00 +0200

    First commit

---
+++ js/background.js
@@ -0,0 +1,24 @@
+var totalSeconds = 0;
+var clicked = false;
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	totalSeconds = request.minutes * 60 + request.seconds;
+	clicked = true;
+});
+
+setInterval(function() {
+	if (totalSeconds > 0) {
+		totalSeconds--;
+		let minutes = Math.floor(totalSeconds / 60);
+		let seconds = totalSeconds % 60;
+		chrome.runtime.sendMessage({minutes, seconds});
+	} else {
+
+		if (clicked) {
+			chrome.tabs.query({}, function (tabs) {
+    			for (let i = 0; i < tabs.length; i++) {
+        			chrome.tabs.remove(tabs[i].id);
+    			}
+			});
+		}
+	}
+}, 1000);
---
+++ js/popup.js
@@ -0,0 +1,22 @@
+
+$(function() {
+
+	$("button").click(function(event) {
+		event.preventDefault();
+		let minutes = parseInt($("input").val());
+		let seconds = 0;
+		$(this).prop("disabled", true);
+
+		chrome.runtime.sendMessage({minutes, seconds});
+	});
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	displayTime(request.minutes, request.seconds);
+});
+
+function displayTime(minutes, seconds) {
+	let m = (minutes < 10) ? "0" + minutes : minutes;
+	let s = (seconds < 10) ? "0" + seconds : seconds;
+	$(".remaining").text(m+":"+s);
+}
