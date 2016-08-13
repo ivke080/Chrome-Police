1c71bb85e88da007918589ae9f5c270ed76a50d9
ivke080 <ivke11080@gmail.com>
2016-08-13 17:03:22 +0200

    complete fix

--- js/background.js
+++ js/background.js
@@ -1,5 +1,7 @@
 var totalSeconds = 0;
 var clicked = false;
+//openTime holds background's minutes and seconds so when popup is opened minutes:seconds is not 00:00
+var openTime = {minutes: 0, seconds: 0};
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	totalSeconds = request.minutes * 60 + request.seconds;
 	clicked = true;
@@ -10,6 +12,9 @@ setInterval(function() {
 		totalSeconds--;
 		let minutes = Math.floor(totalSeconds / 60);
 		let seconds = totalSeconds % 60;
+		// remembering background's time every second. This object is called in popup.js
+		openTime.minutes = minutes;
+		openTime.seconds = seconds;
 		chrome.runtime.sendMessage({minutes, seconds});
 	} else {
 
--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,29 @@
 
 $(function() {
+	// here openTime is called from the background.js with current time in minutes and seconds
+	// without this variable and logic, whenever user opens popup, for a second minutes:seconds is 00:00 and then changes
+	let openTime = chrome.extension.getBackgroundPage().openTime;
+
+	displayTime(openTime.minutes, openTime.seconds);
 
 	$(".start").click(function(event) {
 		event.preventDefault();
 		let minutes = parseInt($("input").val());
+		if (isNaN(minutes) || minutes === 0) {
+			minutes = 1;
+		} else if (minutes > 60) {
+			minutes = 60;
+		}
 		let seconds = 0;
-		$(this).prop("disabled", true);
-
-		chrome.runtime.sendMessage({minutes, seconds});
+		if ($(this).attr("class").indexOf("non-clickable") < 0) {
+			chrome.runtime.sendMessage({minutes, seconds});
+			$("input").val();
+			$(this).addClass("non-clickable");
+		}
 	});
 });
 
+// chrome method for listening messages from the background page
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	displayTime(request.minutes, request.seconds);
 });
